f58628467f5affb2dbcdb3c75b070f9845d5d32d
Vadym <vadymkravtcsov@gmail.com>
2026-03-29 22:58:28 +0300

    fix: model Comment (class Meta)

--- backend/comments/models.py
+++ backend/comments/models.py
@@ -30,4 +30,4 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ["-created_at"]
+        ordering = ["-created_at", "-id"]
