71f57d5f0f146580983afe0086d4e7461a7ec9c7
Vadym <vadymkravtcsov@gmail.com>
2026-03-30 19:16:21 +0300

    feat: register admin site for test Comment model.

---
+++ backend/comments/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Comment
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("nickname", "email", "parent", "created_at")
+    list_filter = ("created_at",)
+    search_fields = ("nickname", "email", "text")
