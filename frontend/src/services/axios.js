eda161a99ba5b3ff6a68071bffa47ab5032bf54b
Vadym <vadymkravtcsov@gmail.com>
2026-03-31 13:22:43 +0300

    style: standardize quotes in axios configuration.

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -1,12 +1,12 @@
 // ----  Settings basic HTTP client (axios)
-import axios from "axios";
+import axios from 'axios'
 
 const apiClient = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
-    headers: {
-        // to send images and TXT files
-        "Content-Type": "multipart/form-data"
-    },
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    // to send images and TXT files
+    'Content-Type': 'multipart/form-data',
+  },
 })
 
 export default apiClient
