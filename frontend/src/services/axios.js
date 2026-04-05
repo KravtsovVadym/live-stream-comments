c0b1411715298c2424cdab25fe1b743a13fd38da
Vadym <vadymkravtcsov@gmail.com>
2026-04-05 09:54:14 +0300

    feat: build API services.

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -3,10 +3,6 @@ import axios from 'axios'
 
 const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  headers: {
-    // to send images and TXT files
-    'Content-Type': 'multipart/form-data',
-  },
   timeout: 15000,
 })
 
--- frontend/src/services/commentService.js
+++ frontend/src/services/commentService.js
@@ -1,18 +1,22 @@
-// ---- (commentService) — API service for comments <
+// ---- (commentService) — API service for comments
 
 import apiClient from './axios'
 
 export const commentService = {
-  // ---- Getting a list of comments (pagination of 25)
+  // ---- List of comments
   getComments(page = 1, ordering = '-created_at') {
     return apiClient.get(`comments/?page=${page}&ordering=${ordering}`)
   },
-  // ---- Getting a CAPTCHA
+  // ---- CAPTCHA
   getCaptcha() {
     return apiClient.get('captcha/')
   },
   // ---- Creation and submitting a comment
   createComment(formData) {
-    return apiClient.post('comints/', formData)
+    return apiClient.post('comments/', formData, {
+      headers: {
+        'content-type': 'multipart/form-data',
+      },
+    })
   },
 }
